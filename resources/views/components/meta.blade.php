e0ddf65aa29a44c2de493f03b673eba5b7f9b128
codenashwan <nashwan.via.ps3@gmail.com>
2025-01-31 02:04:19 +0300

    Update environment configuration and GitHub Actions workflow for deployment

--- resources/views/components/meta.blade.php
+++ resources/views/components/meta.blade.php
@@ -1,13 +1,13 @@
 <title>{{ $meta['title'] ?? env('APP_NAME') }}</title>
 <meta name="title" content="{{ $meta['title'] ?? env('APP_NAME') }}">
-<meta name="description" content="{{ $meta['description'] ?? env('APP_DESCRIPTION')}}">
+<meta name="description" content="{{ $meta['description'] ?? env('APP_DESCRIPTION') }}">
 
 <meta property="og:type" content="website">
 <meta property="og:url" content="{{ $meta['url'] ?? env('APP_URL') }}">
 <meta property="og:title" content="{{ $meta['title'] ?? env('APP_NAME') }}">
-<meta property="og:description" content="{{ $meta['description'] ?? env('APP_DESCRIPTION')}}">
+<meta property="og:description" content="{{ $meta['description'] ?? env('APP_DESCRIPTION') }}">
 <meta property="og:image" content="{{ $meta['image'] ?? asset('img/meta.png') }}">
-<meta property="og:image:alt" content="{{  $meta['title'] ?? env('APP_NAME') }}" />
+<meta property="og:image:alt" content="{{ $meta['title'] ?? env('APP_NAME') }}" />
 <meta property="og:site_name" content="{{ env('APP_NAME') }}" />
 <meta property=" og:locale" content="en_US" />
 
@@ -16,5 +16,5 @@
 <meta property="twitter:url" content="{{ $meta['url'] ?? env('APP_URL') }}">
 <meta property="twitter:image" content="{{ $meta['image'] ?? asset('img/meta.png') }}">
 <meta property="twitter:title" content="{{ $meta['title'] ?? env('APP_NAME') }}">
-<meta property="twitter:description" content="{{ $meta['description'] ??  env('APP_DESCRIPTION')}}">
+<meta property="twitter:description" content="{{ $meta['description'] ?? env('APP_DESCRIPTION') }}">
 <meta name="theme-color" content="#ff541a">
